878e1b37377bfe249410ee1bf8705172595135f9
Brent MacArthur <bmacarthur67@gmail.com>
2019-12-01 18:00:26 -0800

    Add parameters and commands, change S3 file URL.

--- create-pma-stack.ps1
+++ create-pma-stack.ps1
@@ -1,8 +1,13 @@
 $VpcId = "vpc-feb2a199"
 $SubnetId = "subnet-0a889651"
+$InstanceType = "t2.micro"
 aws cloudformation create-stack  `
-     --stack-name rma `
-     --template-url https://rseg176-bmacarthur.s3-us-west-1.amazonaws.com/PMA-CloudFormation.json `
-     --parameters ParameterKey=KeyName,ParameterValue=mykey `
-     "ParameterKey=VPC,ParameterValue=$VpcId" `
-     "ParameterKey=Subnet,ParameterValue=$SubnetId"
+     --stack-name pma `
+     --template-url https://rseg176-bmacarthur.s3-us-west-1.amazonaws.com/pma-server.yaml `
+     --parameters ParameterKey=KeyName, ParameterValue=webserver `
+     ParameterKey=VPC, ParameterValue=$VpcId `
+     ParameterKey=Subnet, ParameterValue=$SubnetId `
+     ParameterKey=InstanceType, ParameterValue=$InstanceType
+
+aws cloudformation wait stack-create-complete --stack-name pma  
+aws cloudformation describe-stacks --stack-name pma --query "Stacks[0].Outputs"
