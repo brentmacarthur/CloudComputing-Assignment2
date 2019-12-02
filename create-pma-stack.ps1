913fa33ec3118a1b7da260f24832f271fa22ddcf
Brent MacArthur <bmacarthur67@gmail.com>
2019-12-01 18:56:28 -0800

    Change KeyName ParameterValue to reference the variable

--- create-pma-stack.ps1
+++ create-pma-stack.ps1
@@ -1,10 +1,11 @@
 $VpcId = "vpc-feb2a199"
 $SubnetId = "subnet-0a889651"
+$KeyName = 'webserver'
 $InstanceType = "t2.micro"
 aws cloudformation create-stack  `
      --stack-name pma `
      --template-url https://rseg176-bmacarthur.s3-us-west-1.amazonaws.com/pma-server.yaml `
-     --parameters ParameterKey=KeyName, ParameterValue=webserver `
+     --parameters ParameterKey=KeyName, ParameterValue=$webserver `
      ParameterKey=VPC, ParameterValue=$VpcId `
      ParameterKey=Subnet, ParameterValue=$SubnetId `
      ParameterKey=InstanceType, ParameterValue=$InstanceType
